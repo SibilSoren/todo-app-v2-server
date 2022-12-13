e4fd55569348dc8fa0505e65946657f0c2ed762b
Sibil Sarjam Soren <sibil@Sibils-MacBook-Air.local>
2022-12-14 01:04:52 +0530

    cors issue

--- api/app.js
+++ api/app.js
@@ -9,6 +9,15 @@ app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 //connecting to DB
 connectDB();
 
